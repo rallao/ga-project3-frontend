80d5748a4e62efcc7e1d4db0cb5890f95833d344
rallao85@gmail.com <rallao85@gmail.com>
2021-11-01 15:53:53 -0300

    timer message fixed

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Countdown from "react-countdown";
 
-const Completionist = () => <span>You can take a break!</span>;
+const Completionist = () => <span>You finish</span>;
 
 const Timer = () => {
   return (
     <div>
-      <h1>This is the Timer</h1>
+      <h1>This is the Pomodoro Timer</h1>
 
       <button>Start Timer</button>
-      <Countdown date={Date.now() + 500000}>
+      <Countdown date={Date.now() + 1500000}>
         <Completionist />
       </Countdown>
     </div>
