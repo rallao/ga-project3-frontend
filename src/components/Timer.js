f60815a5303ca2f61e40af71016b31453862ee68
rallao85@gmail.com <rallao85@gmail.com>
2021-11-01 14:26:01 -0300

    add react-countdowm npm

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Countdown from 'react-countdown'
 
 const Timer = () => {
     return (
         <div>
             <h1>This is the Timer</h1>
+            <button >Start Timer</button>
         </div>
     )
 }
