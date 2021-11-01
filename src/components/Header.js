4507a2ecabe44384740a0b1ec3b7652a02c3b71d
rallao85@gmail.com <rallao85@gmail.com>
2021-11-01 14:23:12 -0300

    add new components to app

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div>
-            <h1>This is the Timer</h1>
+            <h1>This is the Header</h1>
         </div>
     )
 }
