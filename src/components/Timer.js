a49026fde23b60d1afdab97093e1fc1b4136de11
rallao85@gmail.com <rallao85@gmail.com>
2021-11-01 14:46:20 -0300

    add timer component example

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import Countdown from 'react-countdown'
+import React from "react";
+import Countdown from "react-countdown";
+
+const Completionist = () => <span>You can take a break!</span>;
 
 const Timer = () => {
-    return (
-        <div>
-            <h1>This is the Timer</h1>
-            <button >Start Timer</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>This is the Timer</h1>
 
-export default Timer
+      <button>Start Timer</button>
+      <Countdown date={Date.now() + 500000}>
+        <Completionist />
+      </Countdown>
+    </div>
+  );
+};
+
+export default Timer;
