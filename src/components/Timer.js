efd865541ace27d52a60407bc9b2bd4e7a23a589
rallao85@gmail.com <rallao85@gmail.com>
2021-11-01 19:42:22 -0300

    add comments to code

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -9,7 +9,9 @@ const Timer = () => {
       <h1>This is the Pomodoro Timer</h1>
 
       <button>Start Timer</button>
+      {/* set timer to 25:00 min */}
       <Countdown date={Date.now() + 1500000}>
+          {/* when timer is over sent this message */}
         <Completionist />
       </Countdown>
     </div>
