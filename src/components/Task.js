284ce5767abd8cb679706de7f8c43a5b57b144dc
rallao85@gmail.com <rallao85@gmail.com>
2021-11-02 12:44:10 -0300

    wip: new items dds everytime

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+// ID generator for task object
+import { nanoid } from "nanoid";
 
 const Task = () => {
   // add useState to React
   const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
 
   // addTask function.
   const addTask = (e) => {
@@ -16,6 +19,10 @@ const Task = () => {
     }
     // Send the value in console.
     console.log(task);
+    setTasks([...tasks, { id: nanoid(10), taskName: task }]);
+
+    // Clean up the form
+    setTask("");
   };
 
   return (
@@ -23,13 +30,17 @@ const Task = () => {
       <div className="col-8">
         <h4 className="text-center">Task List</h4>
         <ul className="list-group">
-          <li className="list-group-item">
-            <span className="lead">Task Name</span>
-            <button className="btn btn-danger btn-sm float-right mx-2">
-              Delete
-            </button>
-            <button className="btn btn-warning btn-sm float-right">Edit</button>
-          </li>
+          {tasks.map((item) => (
+            <li className="list-group-item">
+              <span className="lead">Task Name</span>
+              <button className="btn btn-danger btn-sm float-right mx-2">
+                Delete
+              </button>
+              <button className="btn btn-warning btn-sm float-right">
+                Edit
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
       <div className="col-4">
@@ -41,6 +52,8 @@ const Task = () => {
             placeholder="New task"
             // handle the user input in realtime with onChange.
             onChange={(e) => setTask(e.target.value)}
+            //
+            value={task}
           />
           <button className="btn btn-dark btn-block" type="submit">
             Add Task
