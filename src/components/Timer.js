627c848c20e7ccee62cb8099109d27768b63236a
rallao85@gmail.com <rallao85@gmail.com>
2021-11-03 14:17:39 -0300

    hide timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,21 +1,21 @@
-import React from "react";
-import Countdown from "react-countdown";
+// import React from "react";
+// import Countdown from "react-countdown";
 
-const Completionist = () => <span>You finish</span>;
+// const Completionist = () => <span>You finish</span>;
 
-const Timer = () => {
-  return (
-    <div className="text-center">
-      <h1>This is the Pomodoro Timer</h1>
+// const Timer = () => {
+//   return (
+//     <div className="text-center">
+//       <h1>This is the Pomodoro Timer</h1>
 
-      <button>Start Timer</button>
-      {/* set timer to 25:00 min */}
-      <Countdown date={Date.now() + 1500000}>
-          {/* when timer is over sent this message */}
-        <Completionist />
-      </Countdown>
-    </div>
-  );
-};
+//       <button>Start Timer</button>
+//       {/* set timer to 25:00 min */}
+//       <Countdown date={Date.now() + 1500000}>
+//           {/* when timer is over sent this message */}
+//         <Completionist />
+//       </Countdown>
+//     </div>
+//   );
+// };
 
-export default Timer;
+// export default Timer;
