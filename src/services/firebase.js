1acfd63eaad9183db49eebe236aa31fa78ac847a
rallao85@gmail.com <rallao85@gmail.com>
2021-11-03 22:13:48 -0300

    wip: add firebase npm and config

---
+++ src/services/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2rqc733yy0EgQrtUn-NZHgiAlBsAnG5A",
+  authDomain: "stay-focused-4e0ea.firebaseapp.com",
+  projectId: "stay-focused-4e0ea",
+  storageBucket: "stay-focused-4e0ea.appspot.com",
+  messagingSenderId: "269731414546",
+  appId: "1:269731414546:web:dffcc879e82c6079538ecb",
+};
+
+firebase.initializeApp(firebaseConfig);
