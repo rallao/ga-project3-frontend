8ff855742964118db63aef95a3cfc72c39cbea6f
rallao85@gmail.com <rallao85@gmail.com>
2021-11-04 12:07:23 -0300

    social login working

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
 import { useState, useEffect } from "react";
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import { auth } from "./services/firebase";
 
 // Import Components
 import Header from "./components/Header";
 // import Timer from "./components/Timer";
-import Task from "./components/Task";
+// import Task from "./components/Task";
 
 // Import Pages
-import Home from './pages/Home'
-import Login from './pages/Login.js'
-import Todolist from './pages/Todolist'
+import Home from "./pages/Home";
+import Login from "./pages/Login.js";
+import Todolist from "./pages/Todolist";
 
 function App() {
+  // Login Feature
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsuscribe = auth.onAuthStateChanged((user) => setUser(user));
+    return () => unsuscribe();
+  }, [user]);
+
+  
   return (
     <div className="container">
-      <Header />
+      <Header user={user} />
       <Switch>
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/todolist">
-          <Todolist />
-          <Task />
-        </Route>
+        <Route
+          path="/login"
+          render={() => (user ? <Redirect to="/todolist" /> : <Login />)}
+        />
+        <Route
+          path="/todolist"
+          render={() => (user ? <Todolist /> : <Redirect to="/login" />)}
+        />
       </Switch>
-      
     </div>
   );
 }
--- src/pages/Todolist.js
+++ src/pages/Todolist.js
@@ -1,10 +1,9 @@
 import Task from "../components/Task";
-import Header from "../components/Header";
 
 const Todolist = (props) => {
     return (
         <div>
-            {/* <Task /> */}
+            <Task />
         </div>
     );
 };
